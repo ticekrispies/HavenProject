9bbd519224d7eab088d0e1a82f0bef87acf7863e
ticekrispies <Wolfje112>
2021-11-22 05:25:35 +0000

    Massive overhaul of activities system (how pages are written/shown). Design page needs placeholder content, preferably interactive if time allows. Gallery page contains images, could use some informative text upon tapping an image or subtitle text overlaying.
    Any code that has been removed (i.e. Iker's home, Ayla's Gallery) can be found in previous commits, should anyone want to revisit or reuse. Sorry for any inconvenience and let me know if you need a hand finding older code.
    Love, Thijs x

---
+++ app/src/main/java/com/standard/acmeatelier/GalleryFragment.java
@@ -0,0 +1,19 @@
+package com.standard.acmeatelier;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+public class GalleryFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_inspire, container, false);
+    }
+}
